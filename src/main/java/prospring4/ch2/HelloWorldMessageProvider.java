c429541c8c57e7f28b8f06170bebcbed6848518d
droopy@pochta.ru <fkf7kfqrf>
2017-08-15 22:56:29 +0700

    Decoupled

---
+++ src/main/java/prospring4/ch2/HelloWorldMessageProvider.java
@@ -0,0 +1,7 @@
+package prospring4.ch2;
+
+public class HelloWorldMessageProvider implements MessageProvider {
+	public String getMessage() {
+		return "Hello World!!!";
+	}
+}
---
+++ src/main/java/prospring4/ch2/MessageProvider.java
@@ -0,0 +1,5 @@
+package prospring4.ch2;
+
+public interface MessageProvider {
+	String getMessage();
+}
