9094665f7aeb0fa5f69f1e252b304562e70dc324
nik96 <droopy@pochta.ru>
2017-08-16 18:39:32 +0700

    eclipse updates, some errors

--- src/main/java/prospring4/ch2/HelloWorldSpringDI.java
+++ src/main/java/prospring4/ch2/HelloWorldSpringDI.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class HelloWorldSpringDI {
 	public static void main(String[] args) {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("META-INF  /spring/app-context.  xml");
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("META-INF/spring/app-context.xml");
 		MessageRenderer mr = ctx.getBean("renderer", MessageRenderer.class);
 		mr.render();
 	}
